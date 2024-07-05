caf36570f22248f53171d0525f26c093b904c641
Romain Laval <60115518+romainlavaldev@users.noreply.github.com>
2024-07-05 16:53:39 +0200

    Update BetterGitHoub.user.js

--- BetterGitHoub.user.js
+++ BetterGitHoub.user.js
@@ -6,7 +6,7 @@
 // @author       You
 // @match        *://github.com/*
 // @icon         https://github.githubassets.com/favicons/favicon.svg
-// @grant        none
+// @grant        GM_addStyle
 
 // @require https://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
@@ -14,22 +14,35 @@
     'use strict';
 
     const fav_projects = [
-
+        
     ];
 
     const lightTheme = $('html').attr('data-color-mode') == 'light';
     const user = $('[data-login]').first().attr('data-login');
 
+    GM_addStyle(`
+.search-input {
+width: unset;
+}
+
+.AppHeader .AppHeader-globalBar .AppHeader-search .AppHeader-search-whenRegular {
+min-width: unset;
+}
+    `);
 
     waitForKeyElements('.AppHeader-globalBar-start', addFavoritesProjects);
 
     waitForKeyElements('.AppHeader-context-full > nav > ul', addPrShortcut);
 
-    waitForKeyElements('#js-issues-toolbar a[href*="/pull"].tooltipped', customPrRows);
+    waitForKeyElements('a[href*="/pull"].tooltipped', customPrRows);
+
+    waitForKeyElements('[aria-label="Issues"] .js-issue-row', customIssueRow);
 
     waitForKeyElements('[class*="TokenTextContainer"]', customTokens);
 
     waitForKeyElements('.board-view-column-card > div', customCards);
+
+    waitForKeyElements('.AppHeader-logo, .header-logo, [href="https://github.com/"]:has(svg), [href="https://github.com"]:has(svg), [href="https://github.com/github"]:has(svg)', changeLogo);
 
     function addFavoritesProjects($container) {
         fav_projects.forEach(e => $container.append(`
@@ -59,7 +72,6 @@
     }
 
     function customPrRows($a) {
-
         // Colors
         if ($a.text().trim() == 'Review required') {
             $a.closest('.js-issue-row').css('background-color', lightTheme ? '#ffbf48' : '#895B06')
@@ -100,6 +112,43 @@
         if ($card.find('img[data-testid="github-avatar"]').attr('alt') == user) {
             $card.css('background-color', lightTheme ? 'lavender' : 'darkslategrey');
         }
+    }
+
+    function customIssueRow($issue) {
+        if ($issue.find('img.avatar-user').attr('alt')?.replace('@', '') == user) {
+            $issue.css('background-color', lightTheme ? 'lavender' : 'darkslategrey');
+        }
+    }
+
+    function changeLogo($logoLink) {
+        $logoLink.empty().append(`
+        <svg height="32" aria-hidden="true" viewBox="0 0 299 299" version="1.1" width="32" data-view-component="true" class="octicon octicon-mark-github v-align-middle color-fg-default">
+            <g transform="translate(0.000000,299.000000) scale(0.100000,-0.100000)"
+fill="#ea5e21" stroke="none">
+            <path d="M1365 2980 c-247 -21 -486 -105 -705 -248 -106 -70 -327 -288 -394
+-390 -120 -183 -208 -398 -242 -597 -22 -134 -22 -367 1 -499 26 -150 78 -306
+114 -340 16 -16 27 -34 25 -41 -9 -23 22 -91 89 -191 72 -110 125 -164 160
+-164 19 0 25 -8 35 -44 9 -35 25 -56 74 -100 113 -99 157 -126 209 -126 44 0
+47 -2 52 -30 5 -24 18 -35 64 -59 218 -109 510 -163 768 -141 362 31 681 181
+935 439 208 211 337 453 401 751 33 150 33 438 1 586 -86 401 -307 729 -640
+953 -139 94 -347 181 -514 215 -73 15 -281 37 -323 34 -11 0 -61 -4 -110 -8z
+m380 -76 c513 -90 954 -475 1109 -970 52 -167 61 -235 61 -444 -1 -176 -3
+-206 -28 -308 -67 -285 -201 -518 -415 -724 -352 -338 -846 -469 -1321 -352
+-112 28 -290 96 -302 116 -20 32 32 137 200 403 204 324 714 1009 1005 1348
+39 47 47 61 35 68 -8 5 -19 9 -25 9 -14 0 -236 -235 -449 -474 -407 -457 -696
+-837 -831 -1091 -41 -77 -46 -92 -42 -136 5 -46 3 -49 -17 -49 -24 0 -112 61
+-178 123 -63 59 -53 91 90 299 205 300 706 813 1102 1130 69 56 158 129 196
+162 39 33 72 62 74 63 3 2 -6 14 -18 27 l-23 25 -134 -90 c-279 -186 -523
+-382 -803 -642 -299 -279 -516 -524 -578 -652 -37 -77 -38 -84 -19 -137 12
+-33 12 -38 -1 -38 -32 0 -55 25 -130 137 -92 138 -96 160 -41 241 139 206 459
+489 790 697 68 43 205 131 305 197 101 66 271 164 378 219 183 94 203 107 189
+121 -19 20 -555 -232 -814 -382 -345 -201 -694 -466 -819 -624 -82 -103 -103
+-166 -76 -229 11 -26 -20 -22 -40 6 -23 33 -71 190 -91 298 -23 126 -23 361 0
+488 90 494 419 900 878 1085 104 42 262 82 373 95 102 12 294 5 410 -15z"/>
+
+            </svg>
+            </g>
+        `);
     }
 
     function sleep(ms) {
